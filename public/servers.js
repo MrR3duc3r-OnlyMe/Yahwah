39845015b0ae345138f5fb2f41a5d8a9da8a492a
MrR3duc3r-OnlyMe <neth@wiegine.dev>
2024-08-18 23:49:58 +0800

    update servers.js

--- public/servers.js
+++ public/servers.js
@@ -1,18 +1,16 @@
 const select = document.getElementById("servers");
-const selectt1 = localStorage.getItem(select.id) - 1;
-async function add(num){
+const selectt1 = parseInt(localStorage.getItem(select.id)) - 1;
+async function add(num, desc){
   num = num.toString();
   const server = document.createElement('option');
   server.value = num;
-  server.text = `Server ${num === "1" ? "1 (Main)" : num}`;
+  server.text = `Server ${num}${desc ? ` — ${desc}` : ``}`;
   select.add(server);
 }
 const tangina = async (t) => {
   localStorage.setItem(select.id, select.options[select.selectedIndex].value.toString() || "1");
   window.location.reload();
 };
-add(1);
-add(2);
-add(3);
+add(1, "Main");
 
-select.value = selectt1;
+select.value = selectt1 < 0 ? "1" : selectt1;
