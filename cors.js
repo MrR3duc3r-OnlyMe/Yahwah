0c6c0b7f453a959e3dc9b9faea570a1f1012db25
MrR3duc3r-OnlyMe <neth@wiegine.dev>
2024-08-27 05:24:33 +0800

    update cors.js

--- cors.js
+++ cors.js
@@ -3,6 +3,7 @@ module.exports = async (request, response, next) => {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, PATCH, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, X-Requested-With, Accept",
+    "Cache-Control": "no-cache, no-store"
   });
 
   if (request.method === 'OPTIONS') {
