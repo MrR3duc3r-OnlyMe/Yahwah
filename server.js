f7ccbe3f5a5a264c1c4bcf523f859256fd232f1f
MrR3duc3r-OnlyMe <neth@wiegine.dev>
2024-09-01 04:18:46 +0800

    update

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ module.exports = [
     description: "Project Botify main server."
   },
   {
-    server: "https://autonomous-karrah-nethproject-a152bbb1.koyeb.app",
-    name: "Koyeb",
-    description: "Fast but experimental. Sometimes it may stop the bot"
-  },
-  {
     server: "https://wieginedev.onrender.com",
     name: "Render",
     description: "New server. same as Main."
@@ -19,11 +14,11 @@ module.exports = [
     name: "Hidencloud",
     description: "New server."
   },
-  /*{
+  {
     server: "http://ap.sg.1.xeh.sh:25098",
     name: "XEH",
-    description: "New and fast server. 2GB RAM | 200% CPU"
-  },*/
+    description: "New and fast server. Upgraded- 300% CPU — 3GB RAM"
+  },
   {
     server: "https://botidebug.onrender.com",
     name: "DebugServer",
