adceae7cbd45a20d257b10aeb53ef1efc1f3bff3
MrR3duc3r-OnlyMe <neth@wiegine.dev>
2024-09-01 12:26:30 +0800

    update encrypt.js

--- encrypt.js
+++ encrypt.js
@@ -1,8 +1,8 @@
 module.exports = {
   encryptData(string) {
-    return Buffer.from(string).toString("hex");
+    return Buffer.from(string).toString("base64");
   },
   decryptData(string) {
-    return Buffer.from(string, "hex").toString("ascii");
+    return Buffer.from(string, "base64").toString("ascii");
   }
 }
