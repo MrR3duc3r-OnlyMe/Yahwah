bec805a8581578f4cdf477a79b90d2c62421432f
MrR3duc3r-OnlyMe <neth@wiegine.dev>
2024-09-04 13:36:56 +0800

    update

--- encrypt.js
+++ encrypt.js
@@ -1,20 +1,8 @@
-const Cryptr = require('cryptr');
-const cryptr = new Cryptr('gagokabahahahahahhahahhha', {
-  encoding: 'base64',
-  pbkdf2Iterations: 10000,
-  saltLength: 64
-});
 module.exports = {
-  /*encryptData(string) {
+  encryptData(string) {
     return Buffer.from(string).toString("base64");
   },
   decryptData(string) {
     return Buffer.from(string, "base64").toString("ascii");
-  }*/
-  encryptData(str){
-    return cryptr.encrypt(str);
-  },
-  decryptData(str){
-    return cryptr.decrypt(str);
   }
 }
