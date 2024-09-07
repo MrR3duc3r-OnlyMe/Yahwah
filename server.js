bea14b3584d7500b4e1eb538edb2bc6438d45179
MrR3duc3r-OnlyMe <neth@wiegine.dev>
2024-09-08 04:40:09 +0800

    update server.js

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ module.exports = [
   {
     server: "http://ap.sg.1.xeh.sh:25098",
     name: "XEH",
-    description: "New and fast server. Upgraded 300% CPU and 3GB RAM"
+    description: "300% CPU | 3GB RAM"
   },
   {
     server: "https://aspintol1.onrender.com",
     name: "Render 1",
-    description: "Project Botify main server."
+    description: ""
   },
   {
     server: "https://wieginedev.onrender.com",
     name: "Render 2",
-    description: "New server. same as Main."
+    description: ""
   },
   {
     server: "https://botidebug.onrender.com",
-    name: "Debug Server",
-    description: "Can access experimental features, beta testing, Use at your own risk, you might suspend your account due to multiple restart on testing"
+    name: "Render 3",
+    description: ""
   }
 ];
